48195d5062bfec7d3c6e7c43710e3e5611a021fb
hubert-kniola <hubert.kniola@gmail.com>
2021-01-08 14:12:27 +0100

    final changes

--- Stegano/stegano.py
+++ Stegano/stegano.py
@@ -4,6 +4,7 @@ from bitstring import BitArray
 import os
 import sys
 
+
 def check_arguments():
     if len(sys.argv) != 2:
         print('Give one argument - name of the file to be encrypted')
@@ -24,13 +25,12 @@ def gen_pixels(infile):
     return image, pixels
 
 
-def encryption(message: str, path: str) -> Image:
+def encryption(message: str, path: str, file_name: str) -> Image:
     fbyte = 8
     encoding = 'utf8'
     img, pixels = gen_pixels(path)
     width, height = img.size
     encoded = np.ndarray(pixels.shape, dtype=np.uint8)
-
 
     msg_bits = [bin(char)[2:].zfill(fbyte) for char in message.encode(encoding)] + ['0' * fbyte]
     bits_gen = (c for c in ''.join(msg_bits))
@@ -39,25 +39,25 @@ def encryption(message: str, path: str) -> Image:
     empty = False
     before = []
     after = []
-    for y in range(width):
-        for x in range(height):
+    for y in range(height):
+        for x in range(width):
             if empty:
-                encoded[x, y] = pixels[x, y]
+                encoded[y, x] = pixels[y, x]
 
             else:
-                rgb_bitarray = rgb_pixels(pixels[x, y])
-                before.append([p for p in pixels[x, y]])
+                rgb_bitarray = rgb_pixels(pixels[y, x])
+                before.append([p for p in pixels[y, x]])
 
                 for color in rgb_bitarray:
                     if not empty and (bit := next(bits_gen, None)):
-                        color.set(bit == '1', -1)
+                        color.set(bit == '1', 4)
 
                     else:
                         empty = True
                         break
 
                 r, g, b = rgb_bitarray
-                encoded[x, y] = (r.uintle, g.uintle, b.uintle)
+                encoded[y, x] = (r.uintle, g.uintle, b.uintle)
                 after.append((r.uintle, g.uintle, b.uintle))
 
     print('Before: ', before)
@@ -68,7 +68,7 @@ def encryption(message: str, path: str) -> Image:
     print('Generated image size: {}'.format(sh1.size))
     print(' ')
     sh1.show()
-    sh1.save('encrypt-obraz.bmp')
+    sh1.save('encrypt-' + file_name + '.bmp')
 
 
 def decryption(path: str) -> str:
@@ -83,19 +83,19 @@ def decryption(path: str) -> str:
     secrets = []
     counter = 0
     temp = ''
-    for y in range(width):
-        for x in range(height):
-            rgb_bitarray = rgb_pixels(pixels[x, y])
+    for y in range(height):
+        for x in range(width):
+            rgb_bitarray = rgb_pixels(pixels[y, x])
 
             for color in rgb_bitarray:
-                temp += '1' if color[-1] else '0'
+                temp += '1' if color[4] else '0'
                 counter += 1
 
                 if counter == fbyte:
                     byte = int(temp, 2)
 
                     if byte == 0:
-                        #print(secrets)
+                        # print(secrets)
                         return bytes(b for b in secrets).decode(encoding)
 
                     secrets += [byte]
@@ -106,7 +106,7 @@ def decryption(path: str) -> str:
 
 if __name__ == '__main__':
     file, e, infile = check_arguments()
-    encryption('To jest zakodowana wiadomość !@#$%^&*()_+1234567890-=', infile)
+    encryption('To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-= To jest zakodowana wiadomość !@#$%^&*()_+1234567890-=', infile, file)
 
     new_path = './encrypt-' + file + '.bmp'
     print(decryption(new_path))
